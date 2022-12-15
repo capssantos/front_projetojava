d95d256c4f5ce02c555dd8634f3f266b4ee802c4
Carlos Santos <carlos.a.a.santos@accenture.com>
2022-12-15 15:22:13 -0300

    Ajuste

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ from flask import Flask, request, session, redirect, url_for, render_template, f
 from service.banco import Api
 import json
 
-main = Flask(__name__)
+app = Flask(__name__)
 api = Api()
 
-@main.route('/')
+@app.route('/')
 def inicio():
     data = api.allagencias()
     return render_template('index.html', data = data)
 
-@main.route('/agencia')
+@app.route('/agencia')
 def allagencias():
     data = api.allagencias()
     return render_template('agencias.html', data = data)
 
 if __name__ == '__main__':
-    main.run(debug=True, port=7000)
+    app.run(debug=True, port=7000)

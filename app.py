461bb9c80df7d61c939424b03d5255d680be44ab
Carlos Santos <carlos.a.a.santos@accenture.com>
2022-12-16 15:31:29 -0300

    Alterações e Adição do Cadastro de Agencia

--- app.py
+++ app.py
@@ -1,23 +1,45 @@
 from flask import Flask, request, session, redirect, url_for, render_template, flash, json, jsonify
 from service.banco import Api
 import json
+from time import sleep
 
 app = Flask(__name__)
+app.secret_key = b'TESTE'
+
 api = Api()
 
 @app.route('/')
 def inicio():
-    data = api.allagencias()
-    return render_template('index.html', data = data)
+    return render_template('index.html', data = api.allagencias())
 
 @app.route('/agencia')
 def allagencias():
-    data = api.allagencias()
-    return render_template('agencias.html', data = data)
+    return render_template('agencias.html', data = api.allagencias())
+
+@app.route('/removeagencia/<id>')
+def removeragencia(id):
+    remove = api.removeragencia(id)
+    if remove['msg'] == 'Agencia deletada com sucesso!':
+        sleep(5)
+        flash('Agencia deletada com sucesso!')
+        
+        return redirect(url_for('allagencias'))
+
+@app.route('/cadastroagencia', methods=['GET', 'POST'])
+def cadastroagencia():
+    if request.method == 'POST':
+        nomeagencia = request.form['nomeAgencia']
+        endereco = request.form['endereco']
+        telefone = request.form['telefone']
+        new = api.registaragencia(nomeagencia, endereco, telefone)
+        flash('Agencia ID:{idagencia}, cadastrada com sucesso!'.format(idagencia = new['idAgencia']))
+        return redirect(url_for('allagencias'))
+
+    return render_template('cadastraragencia.html')
 
 @app.errorhandler(404)
 def error_404(e):
     return '<h1>No source like your request.</h1>', 404
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5555)
+    app.run(debug=True, port=7000)
--- service/banco.py
+++ service/banco.py
@@ -4,9 +4,28 @@ import json
 class Api(object):
     def __init__(self):
         self.url = 'https://backend-java.carlosp.dev/'
+        # self.url = 'http://localhost:8080/'
+        self.headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
         # self.url = 'http://srv-captain--backend-java/'
     
     def allagencias(self):
         res = req.get(url=self.url+'allagencias')
         res = json.loads(res.text)
         return res
+
+    def removeragencia(self, id):
+        res = req.delete(url=self.url+'removeragencia/'+id, headers=self.headers)
+        res = json.loads(res.text)
+        return res
+
+    def registaragencia(self, nomeagencia, endereco, telefone):
+        query = '{\"nomeAgencia\":%(nomeagencia)s, \"endereco\": %(endereco)s, \"telefone\":%(telefone)s }' % {
+            "nomeagencia": json.dumps(nomeagencia),
+            "endereco": json.dumps(endereco),
+            "telefone": json.dumps(telefone)
+        }
+        query = json.loads(query)
+        res = req.post(url=self.url+'cadastraragencia', json=query, headers=self.headers)
+        res = json.loads(res.text)
+        return res
+        

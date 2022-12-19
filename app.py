86d6a8819d3c9b65dbbd14516270d2494a634ef7
Carlos Santos <carlos.a.a.santos@accenture.com>
2022-12-18 23:10:25 -0300

    Mudança estrutura do código e novos formulários

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, session, redirect, url_for, render_template, f
 from service.banco import Api
 import json
 from time import sleep
+from random import *
 
 app = Flask(__name__)
 app.secret_key = b'TESTE'
@@ -11,15 +12,15 @@ api = Api()
 @app.route('/')
 def inicio():
     return render_template('index.html', data = api.allagencias())
-
+#-------------------------------------------------------------
+# START - AGENCIA - ROTAS E CONTROLLERS
 @app.route('/agencia')
 def allagencias():
-    return render_template('agencias.html', data = api.allagencias())
+    return render_template('all.html', data = api.allagencias(), page = 'agencia')
 
 @app.route('/removeagencia/<id>')
 def removeragencia(id):
     remove = api.removeragencia(id)
-    print(remove)
     if remove['msg'] == 'Agencia deletada com sucesso!':
         flash('Agencia deletada com sucesso!')
         
@@ -35,21 +36,107 @@ def cadastroagencia():
         flash('Agencia ID:{idagencia}, cadastrada com sucesso!'.format(idagencia = new['idAgencia']))
         return redirect(url_for('allagencias'))
 
-    return render_template('cadastraragencia.html')
+    return render_template('cadastro.html', page = 'agencia',
+                                            nomeagencia = '',
+                                            endereco = '',
+                                            telefone = '',
+                                            idagencia = '')
 
 @app.route('/cadastroagencia/<id>', methods=['GET', 'POST'])
-def alteraragencia():
+def alteraragencia(id):
 
     if request.method == 'GET':
-        return render_template('cadastraragencia.html')
+        agencia = api.agencia(id)
+        return render_template('cadastro.html', page = 'agencia',
+                                                nomeagencia = agencia['nomeAgencia'],
+                                                endereco = agencia['endereco'],
+                                                telefone = agencia['telefone'],
+                                                idagencia = agencia['idAgencia'])
+
     if request.method == 'POST':
+        idagencia = request.form['idAgencia']
         nomeagencia = request.form['nomeAgencia']
         endereco = request.form['endereco']
         telefone = request.form['telefone']
-        new = api.registaragencia(nomeagencia, endereco, telefone)
-        flash('Agencia ID:{idagencia}, cadastrada com sucesso!'.format(idagencia = new['idAgencia']))
+        new = api.atualizaragencia(idagencia, nomeagencia, endereco, telefone)
+        flash('Agencia ID:{idagencia}, autalizado com sucesso!'.format(idagencia = new['idAgencia']))
         return redirect(url_for('allagencias'))
+# END - AGENCIA - ROTAS E CONTROLLERS
+#-------------------------------------------------------------
+# START - CLIENTE - ROTAS E CONTROLLERS
+@app.route('/cliente')
+def allcliente():
+    return render_template('all.html', data = api.allclientes(), page = 'cliente')
 
+@app.route('/removecliente/<id>')
+def removercliente(id):
+    remove = api.removercliente(id)
+    print(remove)
+    if remove['msg'] == 'Cliente deletado com sucesso!':
+        flash('Cliente deletado com sucesso!')
+        
+        return redirect(url_for('allcliente'))
+
+@app.route('/cadastrocliente', methods=['GET', 'POST'])
+def cadastrocliente():
+    if request.method == 'POST':
+        clientenome = request.form['clienteNome']
+        clientecpf = request.form['clienteCPF']
+        clientefone = request.form['clienteFone']
+        idagencia = request.form['idAgencia']
+        
+        conta = api.registarconta(idagencia, randrange(1000,10000,2), '0.0')
+
+        idcontacorrente = conta['idContaCorrente']
+        
+        new = api.registarcliente(clientenome, clientecpf, clientefone, idcontacorrente, idagencia)
+        flash('Cliente ID:{idagencia}, Conta Numero: {contaCorrenteNumero}, cadastrada com sucesso!'.format(idagencia = new['idCliente'], contaCorrenteNumero = conta['contaCorrenteNumero']))
+        return redirect(url_for('allcliente'))
+
+    return render_template('cadastro.html', page = 'cliente',
+                                            clientenome = '',
+                                            clientecpf = '',
+                                            clientefone = '',
+                                            idcontacorrente = '',
+                                            idagencia = '',
+                                            idcliente = ''
+                                            )
+
+@app.route('/cadastrocliente/<id>', methods=['GET', 'POST'])
+def alterarcliente(id):
+
+    if request.method == 'GET':
+        cliente = api.cliente(id)
+        conta = api.conta(str(cliente['idContaCorrente']))
+        return render_template('cadastro.html', page = 'cliente',
+                                                clientenome = cliente['clienteNome'],
+                                                clientecpf = cliente['clienteCPF'],
+                                                clientefone = cliente['clienteFone'],
+                                                idcontacorrente = cliente['idContaCorrente'],
+                                                idagencia = cliente['idAgencia'],
+                                                idcliente = cliente['idCliente'],
+                                                contacorrentenumero = conta['contaCorrenteNumero'],
+                                                contacorrentesaldo = conta['contaCorrenteSaldo'],
+                                                )
+
+    if request.method == 'POST':
+        idagencia = request.form['idAgencia']
+        nomeagencia = request.form['nomeAgencia']
+        endereco = request.form['endereco']
+        telefone = request.form['telefone']
+        new = api.atualizaragencia(idagencia, nomeagencia, endereco, telefone)
+        flash('Agencia ID:{idagencia}, autalizado com sucesso!'.format(idagencia = new['idAgencia']))
+        return redirect(url_for('allcliente'))
+# END - CLIENTE - ROTAS E CONTROLLERS
+#-------------------------------------------------------------
+# START - CONTA - ROTAS E CONTROLLERS
+
+# END - CONTA - ROTAS E CONTROLLERS
+#-------------------------------------------------------------
+# START - EXTRATO - ROTAS E CONTROLLERS
+
+# END - EXTRATO - ROTAS E CONTROLLERS
+#-------------------------------------------------------------
 @app.errorhandler(404)
 def error_404(e):
     return '<h1>No source like your request.</h1>', 404
--- service/banco.py
+++ service/banco.py
@@ -8,6 +8,7 @@ class Api(object):
         self.headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
         # self.url = 'http://srv-captain--backend-java/'
     
+    # START - AGENCIA - ROTAS E CONTROLLERS
     def allagencias(self):
         res = req.get(url=self.url+'allagencias')
         res = json.loads(res.text)
@@ -28,4 +29,74 @@ class Api(object):
         res = req.post(url=self.url+'cadastraragencia', json=query, headers=self.headers)
         res = json.loads(res.text)
         return res
-        
+    
+    def agencia(self, id_agencia):
+        res = req.get(url=self.url+'agencia/'+ id_agencia)
+        res = json.loads(res.text)
+        return res
+
+    def atualizaragencia(self, idagencia, nomeagencia, endereco, telefone):
+        query = '{ \"idAgencia\":%(idAgencia)s, \"nomeAgencia\":%(nomeagencia)s, \"endereco\": %(endereco)s, \"telefone\":%(telefone)s }' % {
+            "idAgencia": json.dumps(idagencia),
+            "nomeagencia": json.dumps(nomeagencia),
+            "endereco": json.dumps(endereco),
+            "telefone": json.dumps(telefone)
+        }
+        query = json.loads(query)
+        res = req.put(url=self.url+'alteraragencia', json=query, headers=self.headers)
+        res = json.loads(res.text)
+        return res
+    # END - AGENCIA - ROTAS E CONTROLLERS
+    # START - CLIENTE - ROTAS E CONTROLLERS
+    def allclientes(self):
+        res = req.get(url=self.url+'allclientes')
+        res = json.loads(res.text)
+        return res
+
+    def removercliente(self, id):
+        res = req.delete(url=self.url+'removercliente/'+id, headers=self.headers)
+        res = json.loads(res.text)
+        return res
+
+    def registarcliente(self, clientenome, clientecpf, clientefone, idcontacorrente, idagencia):
+        query = '{ \"clienteNome\":%(clientenome)s, \"clienteCPF\": %(clientecpf)s, \"clienteFone\":%(clientefone)s, \"idContaCorrente\":%(idcontacorrente)s, \"idAgencia\":%(idagencia)s  }' % {
+            "clientenome": json.dumps(clientenome),
+            "clientecpf": json.dumps(clientecpf),
+            "clientefone": json.dumps(clientefone),
+            "idcontacorrente": json.dumps(idcontacorrente),
+            "idagencia": json.dumps(idagencia),
+            "idagencia": json.dumps(idagencia)
+        }
+        query = json.loads(query)
+        res = req.post(url=self.url+'cadastrarcliente', json=query, headers=self.headers)
+        res = json.loads(res.text)
+        return res
+
+    def cliente(self, id_cliente):
+        res = req.get(url=self.url+'cliente/'+ id_cliente)
+        res = json.loads(res.text)
+        return res
+
+
+    # END - CLIENTE - ROTAS E CONTROLLERS
+    # START - CONTA - ROTAS E CONTROLLERS
+    def registarconta(self, idagencia, contacorrentenumero, contacorrentesaldo):
+        query = '{\"idAgencia\":%(idagencia)s, \"contaCorrenteNumero\": %(contacorrentenumero)s, \"contaCorrenteSaldo\":%(contacorrentesaldo)s }' % {
+            "idagencia": json.dumps(idagencia),
+            "contacorrentenumero": json.dumps(contacorrentenumero),
+            "contacorrentesaldo": json.dumps(contacorrentesaldo)
+        }
+        query = json.loads(query)
+        res = req.post(url=self.url+'cadastratarconta', json=query, headers=self.headers)
+        res = json.loads(res.text)
+        return res
+
+    def conta(self, id_conta):
+        res = req.get(url=self.url+'conta/'+ id_conta)
+        res = json.loads(res.text)
+        return res
+
+    # END - CONTA - ROTAS E CONTROLLERS
+    # START - EXTRATO - ROTAS E CONTROLLERS
+
+    # END - EXTRATO - ROTAS E CONTROLLERS
